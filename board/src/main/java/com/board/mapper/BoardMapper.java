90dc4dceeb3553f83ee298cf32e28eea490436e1
junheeyoon <enteryjh@ajou.ac.kr>
2019-08-28 19:36:33 +0900

    게시판

---
+++ board/src/main/java/com/board/mapper/BoardMapper.java
@@ -0,0 +1,16 @@
+package com.board.mapper;
+
+import java.util.List;
+ 
+import com.board.domain.BoardVO;
+ 
+public interface BoardMapper {
+ 
+    //글작성
+    public void boardInsert(BoardVO vo)throws Exception;
+    
+    //글목록
+    public List<BoardVO>boardList()throws Exception; //추가
+    
+}
+ 
